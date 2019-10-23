cc641179896a6ea505902802fae11b8873daa416
DESPEL <52083283+DESPEL@users.noreply.github.com>
2019-10-22 22:24:17 -0700

    Update

--- cpp/geneticalgs/nqueens.cpp
+++ cpp/geneticalgs/nqueens.cpp
@@ -7,9 +7,10 @@
 
 using namespace std;
 
-float mutationRatio = 0.10;
-float preserveBest = 0.50;
-float preverveRandom = 0.30;
+const float mutationRatio = 0.10;
+const float preserveBest = 0.50;
+const float preverveRandom = 0.30;
+const int popSize = 50;
 
 struct coord {
     coord(int px, int py) { x = px, y = py; }
@@ -36,45 +37,47 @@ void mutate(std::vector<coord> &individual) {
 }
 
 float performance(int nqueens, vector<coord> &individual) {
-    std::vector<vector<bool>> queens(nqueens,
-                                     std::vector<bool>(nqueens, false));
+    std::vector<std::vector<bool>> queens(nqueens,
+                                          std::vector<bool>(nqueens, false));
     int bad = 0;
     for (coord &gene : individual) {
         queens[gene.x][gene.y] = true;
     }
     for (coord &gene : individual) {
         for (int i = 0; i < nqueens; i++) {
-            if (queens[gene.x][i] && i != gene.y) {
+            if (queens[gene.x][i] && i != gene.x) {
                 bad++;
-                continue;
+                break;
             }
             if (queens[i][gene.y] && i != gene.y) {
                 bad++;
-                continue;
+                break;
             }
-            if (gene.x + i < nqueens && gene.y + i < nqueens &&
+            if (gene.x + i < nqueens && gene.x + i != gene.x &&
+                gene.y - i != gene.y && gene.y + i < nqueens &&
                 queens[gene.x + i][gene.y + i]) {
                 bad++;
-                continue;
+                break;
             }
-            if (gene.x - i >= 0 && gene.y - i >= 0 &&
+            if (gene.x - i >= 0 && gene.x - i != gene.x &&
+                gene.y - i != gene.y && gene.y - i >= 0 &&
                 queens[gene.x - i][gene.y - i]) {
                 bad++;
-                continue;
+                break;
             }
             if (gene.x - i >= 0 && gene.y + i < nqueens &&
                 queens[gene.x - i][gene.y + i]) {
                 bad++;
-                continue;
+                break;
             }
             if (gene.x + i < nqueens && gene.y - i >= 0 &&
                 queens[gene.x + i][gene.y - i]) {
                 bad++;
-                continue;
+                break;
             }
         }
     }
-    return (nqueens - bad) / nqueens;
+    return (float)(nqueens - bad) / nqueens;
 }
 
 std::vector<std::vector<coord>> generatePopulation(int size, int boardSize) {
@@ -84,6 +87,15 @@ std::vector<std::vector<coord>> generatePopulation(int size, int boardSize) {
                  individual = generateIndividual(boardSize);
              });
     return population;
+}
+
+std::vector<coord> reproduce(std::vector<coord> &parent,
+                             std::vector<coord> &mother) {
+    int idx = random(0, parent.size() - 1);
+    std::vector<coord> child;
+    child.insert(begin(child), begin(parent), begin(parent) + idx);
+    child.insert(begin(child), begin(mother) + idx, end(mother));
+    return child;
 }
 
 std::vector<std::vector<coord>> derivePopulation(
@@ -107,19 +119,40 @@ std::vector<std::vector<coord>> derivePopulation(
          });
 
     std::vector<std::vector<coord>> result;
-    std::vector<bool> selected(derivePopulation.size());
-    for (int i = 0; i < derivePopulation.size() * preserveBest; i++) {
+    std::vector<bool> selected(parent.size(), false);
+
+    for (int i = 0; i < parent.size() * preserveBest; i++) {
         result.push_back(parent[performances[i].idx]);
+        selected[i] = true;
     }
-    for (int i = 0; i < derivePopulation.size() * preverveRandom; i++) {
 
+    for (int i = 0; i < parent.size() * preverveRandom; i++) {
+        int pos = random(0, parent.size() - 1);
+        while (selected[pos]) pos = random(0, parent.size() - 1);
+        selected[pos] = true;
+        result.push_back(parent[performances[pos].idx]);
     }
+
+    while (result.size() < parent.size()) {
+        int idx1 = random(0, result.size() - 1);
+        int idx2 = random(0, result.size() - 1);
+        result.push_back(reproduce(result[idx1], result[idx2]));
+    }
+
+    for (int i = 0; i < result.size(); i++) {
+        if (random(0, 100) < ceil(mutationRatio * 100))
+            mutate(result[i]);
+    }
+    return result;
 }
-
-// TODO: reproduce, derive population
 // loop solver
 
 int main() {
     int boardSize;
-    cin >> boardSize;
+    std::cin >> boardSize;
+    std::vector<vector<coord>> reinas = generatePopulation(popSize, boardSize);
+    while (performance(boardSize, reinas[0]) != 1) {
+        std::cout << performance(boardSize, reinas[0]) << '\n';
+        reinas = derivePopulation(reinas);
+    }
 }
