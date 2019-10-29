038f68369e1d2ffa3a5951031e2e5add086d42cc
DESPEL <52083283+DESPEL@users.noreply.github.com>
2019-10-28 21:31:01 -0700

    Great Solution

--- practice/HackerRank/StringReduction.cpp
+++ practice/HackerRank/StringReduction.cpp
@@ -5,51 +5,51 @@
 
 using namespace std;
 
-int minsz(string s) {
-    cout << s << '\n';
+pair<int, bool> minsz(string s) {
     if (s.size() == 2) {
         if (s[0] != s[1]) {
-            return 1;
+            return make_pair(1, true);
         } else {
-            return 2;
+            return make_pair(2, true);
         }
     }
     string temp = "";
     string s2 = s;
     s2.erase(begin(s2));
-    int tsz;
+    pair<int, bool> tsz;
     int min = numeric_limits<int>::max();
     bool changed = false;
     for (int i = 1; i < s.size(); i++) {
         s2.erase(begin(s2));
-        if (temp.back() != s2.front()) {
-            if (s[i - 1] != s[i]) {
-                char t = 'a';
-                if ((s[i - 1] == 'a' && s[i] == 'b') ||
-                    (s[i] == 'a' && s[i - 1] == 'b'))
-                    t = 'c';
-                else if ((s[i - 1] == 'b' && s[i] == 'c') ||
-                         (s[i] == 'b' && s[i - 1] == 'c'))
-                    t = 'a';
-                else if ((s[i - 1] == 'a' && s[i] == 'c') ||
-                         (s[i] == 'a' && s[i - 1] == 'c'))
-                    t = 'b';
-                tsz = minsz(temp + t + s2);
-                if (tsz < min) {
-                    cout << tsz << '\n';
-                    min = tsz;
-                }
+        if (s[i - 1] != s[i]) {
+            char t = 'a';
+            if ((s[i - 1] == 'a' && s[i] == 'b') ||
+                (s[i] == 'a' && s[i - 1] == 'b'))
+                t = 'c';
+            else if ((s[i - 1] == 'b' && s[i] == 'c') ||
+                     (s[i] == 'b' && s[i - 1] == 'c'))
+                t = 'a';
+            else if ((s[i - 1] == 'a' && s[i] == 'c') ||
+                     (s[i] == 'a' && s[i - 1] == 'c'))
+                t = 'b';
+            tsz = minsz(temp + t + s2);
+            if (tsz.second == true) {
+                return tsz;
             }
+            if (tsz.first < min) {
+                min = tsz.first;
+            }
+
             changed = true;
         }
         temp += s[i - 1];
     }
     if (!changed)
-        return s.size();
-    return min;
+        return make_pair(s.size(), false);
+    return make_pair(min, false);
 }
 
-int strred(string &s) { return minsz(s); }
+int strred(string &s) { return minsz(s).first; }
 
 int main() {
     int t;
