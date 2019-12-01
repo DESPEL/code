27dd24607e000d537c789a5c6a4d131c8b542244
DESPEL <52083283+DESPEL@users.noreply.github.com>
2019-11-30 18:59:28 -0800

    Update InputEngine.cpp

--- projects/DMSC_Console/InputEngine.cpp
+++ projects/DMSC_Console/InputEngine.cpp
@@ -30,34 +30,24 @@ InputEngine* InputEngine::getInstance() {
 }
 
 void InputEngine::readEvents() {
-	//cout << "read events called" << '\n';
-
 	DWORD cNumRead, fdwMode, i;
 	INPUT_RECORD irInBuf[128];
 	int counter = 0;
-
-	// Get the standard input handle. 
 
 	hStdin = GetStdHandle(STD_INPUT_HANDLE);
 	if (hStdin == INVALID_HANDLE_VALUE)
 		ErrorExit(LPSTR("GetStdHandle"));
 
-	// Save the current input mode, to be restored on exit. 
-
 	if (!GetConsoleMode(hStdin, &fdwSaveOldMode))
 		ErrorExit(LPSTR("GetConsoleMode"));
-
-	// Enable the window and mouse input events. 
 
 	fdwMode = ENABLE_EXTENDED_FLAGS | ENABLE_WINDOW_INPUT | ENABLE_MOUSE_INPUT;;
 	if (!SetConsoleMode(hStdin, fdwMode))
 		ErrorExit(LPSTR("SetConsoleMode"));
 
-	// Loop to read and handle the next 100 input events. 
 	while (true)
 	{
 		// Wait for the events. 
-
 		if (!ReadConsoleInput(
 			hStdin,      // input buffer handle 
 			irInBuf,     // buffer to read into 
@@ -160,7 +150,6 @@ void InputEngine::MouseEventProc(MOUSE_EVENT_RECORD mer)
 	case MOUSE_WHEELED:
 		break;
 	default:
-		//printf("unknown\n");
 		break;
 	}
 }
